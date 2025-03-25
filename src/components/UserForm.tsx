b5079afbf4c260c491f2fa624b8d5fbb6338a7ab
unknown <moonkij@pittasoft.com>
2025-03-25 13:55:05 +0900

    구글 주소

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -9,6 +9,7 @@ import { UserCategory, COLLECTIONS } from "types/schema";
 import { getUserById, getUserCategories, getProductsByCategory } from 'utils/firebase';
 import { User, Product } from 'types/schema';
 import Loader from "./Loader";
+import AddressInput from "./common/AddressInput";
 
 interface UserFormData {
   email: string;
@@ -501,13 +502,11 @@ export default function UserForm() {
                 <label htmlFor="companyAddress" className="block text-sm font-medium text-gray-500">
                   Company Address
                 </label>
-                <input
-                  type="text"
-                  id="companyAddress"
+                <AddressInput
                   value={companyAddress}
-                  onChange={(e) => setCompanyAddress(e.target.value)}
-                  className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                  required
+                  onChange={setCompanyAddress}
+                  placeholder="Enter company address"
+                  className="w-full"
                 />
               </div>
 
